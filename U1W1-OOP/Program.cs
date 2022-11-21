839de70392ad024902cf1d45839a70ff4e4bdd76
RobDab <roberto.dabb@gmail.com>
2022-11-21 17:57:13 +0100

    Aggiungere i file di progetto.

---
+++ U1W1-OOP/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1W1_OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veicolo auto = new Veicolo();
+
+            auto.Modello = "ModelX";
+
+            Console.WriteLine($"Quella {auto.marca} è una {auto.Modello}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ U1W1-OOP/classes/Veicolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U1W1_OOP
+{
+    internal class Veicolo
+    {
+        public string marca = "Tesla";
+        private string _modello;
+
+        public string Modello
+        {
+            get { return _modello; }
+            set { _modello = value; }
+        }
+
+    }
+}
